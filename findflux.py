e7ca87e4ae4f4202843fc1e8dfe08a0ff16a04e7
Nicholas Saunders <nksaunders1994@gmail.com>
2017-06-28 14:26:07 -0700

    tweaks

--- findflux.py
+++ findflux.py
@@ -2,7 +2,10 @@ from everest.math import SavGol
 import everest
 import numpy as np
 
-ID = 212272644
+ID = 212278696
+
+# 9th: 212273934
+# 10th: 212278696
 
 star = everest.Everest(ID)
 time = star.apply_mask(star.time)
