94226c810af74333f67ca7e9d894ff0564f861cc
Nicholas Saunders <nksaunders1994@gmail.com>
2017-07-28 21:13:14 -0700

    CDPP function

--- mkplot.py
+++ mkplot.py
@@ -3,6 +3,7 @@ import numpy as np
 import simulateK2 as sK2
 import aperturefit as af
 import sputter as sp
+from tqdm import tqdm
 
 def mkplot(mag, motion):
 
@@ -17,4 +18,26 @@ def mkplot(mag, motion):
 
     pl.show()
 
-mkplot(10, 20)
+def rawCDPP(mag,motion):
+
+    MN = sp.MotionNoise()
+    flux,rawflux = MN.DetrendFpix(mag,motion)
+
+    rawCDPP = MN.CDPP(rawflux)
+
+    return rawCDPP
+
+sk2 = sK2.Target(205998445)
+sk2.Transit()
+sk2.GeneratePSF(159000.0)
+
+fullcdpp = []
+for m in tqdm(range(5)):
+    mag = m + 10
+    cdpp = []
+    for mot in tqdm(range(20)):
+        motion = mot+1
+        cdpp.append(rawCDPP(mag,motion))
+    fullcdpp.append(cdpp)
+
+print(fullcdpp)
