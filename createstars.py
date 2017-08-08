c6a5781ccec00ffe1ea6b2ad22ad2f52bf70c2cd
Nicholas Saunders <nksaunders1994@gmail.com>
2017-08-08 11:31:24 -0700

    tweaks

--- createstars.py
+++ createstars.py
@@ -11,7 +11,18 @@ def Simulate(ID, f_mag, m_mag):
     fpix, target, ferr = sK2.GeneratePSF(f_mag, motion_mag = m_mag)
 
     return fpix
+'''
+fval = [159000.0, 68000.0, 28000.0]
+mags = [12, 13, 14]
 
-for i in tqdm(range(21)):
-    fpix = Simulate(205998445, 1150000.0, i)
-    np.savez(('stars/neighbor_mag10motion%.i'%i),fpix=fpix)
+for index,m in tqdm(enumerate(mags)):
+    for i in tqdm(range(26)):
+
+        fpix = Simulate(205998445, fval[index], i)
+        np.savez(('stars/larger_aperture/neighbor_mag%imotion%.i'%(m,i)),fpix=fpix)
+'''
+
+for i in tqdm(range(26)):
+
+    fpix = Simulate(205998445, 28000.0, i)
+    np.savez(('stars/larger_aperture/mag14motion%i'%i),fpix=fpix)
