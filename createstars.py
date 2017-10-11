67c5a939a2539cb4da5605469ce6fa75a1323fd5
Nicholas Saunders <nksaunders1994@gmail.com>
2017-10-10 17:24:41 -0700

    creating single lc

--- createstars.py
+++ createstars.py
@@ -2,13 +2,13 @@ import numpy as np
 import simulateK2
 from tqdm import tqdm
 
-def Simulate(ID, f_mag, m_mag):
+def Simulate(ID, mag, m_mag):
 
     sK2 = simulateK2.Target(ID)
     sK2.Transit()
 
     # generate a simulated PSF
-    fpix, target, ferr = sK2.GeneratePSF(f_mag, motion_mag = m_mag)
+    fpix, target, ferr = sK2.GeneratePSF(mag, motion_mag = m_mag)
 
     return fpix
 '''
@@ -22,7 +22,7 @@ for index,m in tqdm(enumerate(mags)):
         np.savez(('stars/larger_aperture/neighbor_mag%imotion%.i'%(m,i)),fpix=fpix)
 '''
 
-for i in tqdm(range(26)):
+fpix = Simulate(205998445, 15., 0)
+np.savez('stars/tests/mag11motion0',fpix=fpix)
 
-    fpix = Simulate(205998445, 28000.0, i)
-    np.savez(('stars/larger_aperture/mag14motion%i'%i),fpix=fpix)
+import pdb; pdb.set_trace()
