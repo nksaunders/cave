26653d44f04e928a5249926d4eb7880c5dd80bd1
Nicholas Saunders <nksaunders1994@gmail.com>
2017-10-20 17:56:23 -0700

    website animation

--- animatemotion.py
+++ animatemotion.py
@@ -11,25 +11,21 @@ from tqdm import tqdm
 ID = 205998445
 
 # simulated a star, takes an ID and flux value (corresponding to magnitude)
-sK2 = simulateK2.Target(int(ID), 159000.0)
+sK2 = simulateK2.Target(int(ID),npts = 100)
 trn = sK2.Transit()
-aft = af.ApertureFit(trn)
 
-fpix, target, ferr = sK2.GeneratePSF(motion_mag = 5)
+fpix, target, ferr = sK2.GeneratePSF(12,motion_mag = 5)
 
 t = np.linspace(0,90,len(fpix))
 
 def images(i):
     fpix_im = pl.imshow(fpix[i],cmap='viridis',origin='lower',interpolation='nearest')
-    pl.title('5x Motion')
-    pl.annotate(r'$\mathrm{Time}: %.2f \mathrm{\ days}$' % (t[i]),
-                    xy = (0.72, 0.05),xycoords='axes fraction',
-                    color='k', fontsize=12, bbox=dict(facecolor='w', edgecolor='k'));
+    pl.axis('off')
     return fpix_im
 
 fig = pl.figure()
-ani = animation.FuncAnimation(fig, images, frames=750, interval = 1, repeat=True)
+ani = animation.FuncAnimation(fig, images, frames=100, interval = 1, repeat=True)
 Writer = animation.writers['ffmpeg']
 writer = Writer(fps = 15, metadata = dict(artist = 'Nicholas Saunders'), bitrate = 1800)
-ani.save('motion5.mp4', writer=writer)
+ani.save('motiontest.mp4', writer=writer)
 pl.show()
